577b8f62b4a7f499fe72e5de18a8235822e48b57
mamamia5x <dorgpay@kmsd.edu>
2021-01-15 18:05:49 +0000

    Update Command

--- commands/index.php
+++ commands/index.php
@@ -1,6 +1,6 @@
 <?php
 $runningVersion = file_get_contents('../version.txt');
-$currentVersion = file_get_contents('https://raw.githubusercontent.com/mamamia5x/Git-Download/main/version.txt');
+$currentVersion = file_get_contents('https://raw.githubusercontent.com/mamamia5x/Git-Download/update/version.txt');
 if ($runningVersion !== $currentVersion){
     echo ("You are running on $runningVersion\nThe current version is $currentVersion.");
     echo ("You can update using the command 'php index.php update'");
@@ -9,7 +9,14 @@ if ($runningVersion !== $currentVersion){
 }
 $runType = $argv[1]; 
 if ($runType == 'update'){
-    echo("Running this command will delete everything in here and reinstall it, make sure you have no other files in here");
+    echo("Running this command will delete everything in here and reinstall it, make sure you have no other files in here.\n");
+    echo("Do you wish to continue?");
+    $response = readline(" > ");
+    if(substr($response,0,1) == 'y'){
+        update();
+    } else {
+        echo "Update Cancelled\n";
+    }
 }
 else if ($runType == "zip"){
     $zipFile = file_get_contents($argv[2]);
@@ -36,5 +43,9 @@ else if ($runType == "zip"){
 }
 else {
     echo "\nError: Command not found.";
+}
+
+function update(){
+    exec('git -c http.sslVerify=false clone https://github.com/mamamia5x/Git-Download.git ../../Git-Download');
 }
 ?>
